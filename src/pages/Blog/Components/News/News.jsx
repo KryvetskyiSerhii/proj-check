2bdec2ba95eefff7a63a97e0711d849e3cedb752
Kryvetskyi Serhii <sam.hartman@mail.ru>
2022-08-29 18:10:43 +0200

    check the bonobo

--- src/pages/Blog/Components/News/News.jsx
+++ src/pages/Blog/Components/News/News.jsx
@@ -1,10 +1,9 @@
 import { useSelector } from 'react-redux'
-import { Article } from './Article'
+
 import React from 'react'
 import { SimpleBlock, Container } from 'components/common/CommonStyled.styled'
 import { NewsBlocks } from 'pages/Blog/Styles/news/NewsStyled.styled'
-import { MenuLink } from 'components/common/menu/MenuLink.styled'
-import { ARTICLES } from 'constants/articlesList'
+
 import { NewsBlockContainer } from 'pages/Blog/Styles/BlogStyled.styled'
 import { OldNews } from './OldNews'
 import { NewsSlider } from './NewsSlider'
@@ -15,16 +14,6 @@ export const News = () => {
       <Container width='80vw'>
         <NewsBlockContainer>
           <NewsBlocks>
-            {/* {articles.map(element => (
-              <MenuLink color='#000' to={element.link}>
-                <Article
-                  id={element.id}
-                  title={element.title}
-                  text={element.shortText}
-                  img={element.img}
-                />
-              </MenuLink>
-            ))} */}
             <NewsSlider />
           </NewsBlocks>
           <OldNews />
